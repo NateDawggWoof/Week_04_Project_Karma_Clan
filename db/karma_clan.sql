28435a384950d3c376e20016c8407c7033dab3f4
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-08-13 19:35:43 +0100

    database connected to sql file

--- db/karma_clan.sql
+++ db/karma_clan.sql
@@ -3,24 +3,24 @@ DROP TABLE actions;
 DROP TABLE users;
 
 
-CREATE TABLE users(
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     goal_daily INT,
     total_daily INT,
-    total_overall INT,
+    total_overall INT
 );
 
-CREATE TABLE  actions(
+CREATE TABLE  actions (
     id SERIAL PRIMARY KEY,
     name VARCHAR (255),
     description TEXT,
-    type VARCHAR(255),
+    type VARCHAR(255)
 );
 
-CREATE TABLE deeds(
+CREATE TABLE deeds (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id),
     action_id INT REFERENCES actions(id),
-    date VARCHAR (255),
-)
+    date VARCHAR (255)
+);
