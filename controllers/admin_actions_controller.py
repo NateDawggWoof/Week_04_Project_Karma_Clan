95a1ce78f2e6e2a99dcbe60200fa9afc93f62fe8
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-08-16 15:13:59 +0100

    add action in html passed

--- controllers/admin_actions_controller.py
+++ controllers/admin_actions_controller.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, Blueprint
 
-from models.user import User
+from models.action import Action
 import repositories.user_repository as user_repo
 import repositories.deed_repository as deed_repo
 import repositories.action_repository as action_repo
@@ -16,4 +16,18 @@ def admin():
 def user_delete(id):
     deed_repo.delete_all_action_deeds(id)
     action_repo.delete(id)
+    return redirect('/admin/actions')
+
+
+@admin_actions_blueprint.route("/admin/actions/new", methods=['POST'])
+def new_action():
+    action_name = request.form['action_name']
+    action_description = request.form['action_description']
+    action_type = request.form['action_type']
+    action_value = request.form['action_value']
+    
+    action = Action(action_name, action_description,action_type, action_value)
+    
+    action_repo.save(action)
+    
     return redirect('/admin/actions')    
