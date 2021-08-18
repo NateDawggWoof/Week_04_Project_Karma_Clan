310ceae14ee1f8345d7c7c9819c67a65b3cbe4a5
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-08-18 15:31:04 +0100

    get a list of most occouring fron list function completed in test file

--- models/test.py
+++ models/test.py
@@ -1,6 +1,6 @@
 from collections import Counter
-data_list = [1,1,1,1,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4]
-data_list2 = [1,1,1,1,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4]
+data_list = [1,1,1,1,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4,7,7,7,7,7,7,7]
+data_list2 = [1,1,1,1,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4,7,7,7,7,7,7,7]
 least_common = Counter(data_list).most_common()[-1]
 most_common = Counter(data_list).values()
 # print(least_common)
@@ -27,5 +27,28 @@ def least(data_list1,data_list2):
 
 test_1 = least(data_list,data_list2)
 
-print(test_1)
+# print(test_1)
 
+def most(data_list1,data_list2):
+    data_total = data_list1 + data_list2
+    data_list = data_total.copy()
+    counter = 0
+    most_common_list = []
+
+    for data in data_total:
+        most_common = Counter(data_list).most_common()[0]
+        # print(least_common_list)
+        # print(data_list)
+        # print(most_common[1])
+        print(most_common)
+        if most_common[1] >= counter:
+            most_common_list.append(most_common[0])
+            counter = most_common[1]
+            # mylist = list(filter((r_item).__ne__, mylist))
+            # data_list.remove(least_common[0])
+            data_list = list(filter((most_common[0]).__ne__,data_list))
+    return  most_common_list
+
+test_2 = most(data_list,data_list2)
+
+print(test_2)
